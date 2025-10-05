2fc27986952b78e8d51783d2a2cf32d52d627cbb
krishna <krishnasharmacit@gmail.com>
2025-10-05 23:34:03 +0530

    minor fixes 2

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173,
+    allowedHosts: ['portfolio-r9b1.onrender.com'], // 👈 add your Render domain here
+  },
 })
