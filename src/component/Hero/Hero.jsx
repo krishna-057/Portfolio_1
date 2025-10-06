7231c20af2b4914172f37b0a2b19bc967f3aaf29
krishna <krishnasharmacit@gmail.com>
2025-10-07 04:14:52 +0530

    hero section background animation

--- src/component/Hero/Hero.jsx
+++ src/component/Hero/Hero.jsx
@@ -5,24 +5,31 @@ function Hero() {
   return (
     <section className="hero" id="home">
       {/* <div className="hero"> */}
-        <div className="hero-bg"></div>
-        <div className="hero-content container">
-          <h1 className="hero-title">Krishna Sharma</h1>
-          <h2 className="hero-subtitle">Full Stack Developer</h2>
-          <p className="hero-description">
-            I’m a full stack developer who loves turning ideas into real,
-            working web apps. From front-end design to back-end logic, I enjoy
-            building things that people actually use.
-          </p>
-          <div className="cta-buttons">
-            <a href="#projects" className="btn btn-primary">
-              View Projects
-            </a>
-            <a href="https://drive.google.com/uc?export=download&id=10U73wDPw3OmC0rGahWnWibABXVSnJeti" className="btn btn-secondary">
-              Download Resume
-            </a>
-          </div>
+      <div className="hero-bg">
+        <div className="animated cir1"></div>
+        <div className="animated cir2"></div>
+        <div className="animated cir3"></div>
+      </div>
+      <div className="hero-content container">
+        <h1 className="hero-title">Krishna Sharma</h1>
+        <h2 className="hero-subtitle">Full Stack Developer</h2>
+        <p className="hero-description">
+          I’m a full stack developer who loves turning ideas into real, working
+          web apps. From front-end design to back-end logic, I enjoy building
+          things that people actually use.
+        </p>
+        <div className="cta-buttons">
+          <a href="#projects" className="btn btn-primary">
+            View Projects
+          </a>
+          <a
+            href="https://drive.google.com/uc?export=download&id=10U73wDPw3OmC0rGahWnWibABXVSnJeti"
+            className="btn btn-secondary"
+          >
+            Download Resume
+          </a>
         </div>
+      </div>
       {/* </div> */}
     </section>
   );
